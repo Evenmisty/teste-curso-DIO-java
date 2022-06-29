a814467f6b84c6e592fa4cad5aac5ac1a48e75b7
Even <palomamribeiro27@gmail.com>
2022-06-29 18:09:00 -0300

    first commit

---
+++ src/com/even/PrimeiroProjetoJava.java
@@ -0,0 +1,14 @@
+package com.even;
+
+import com.even.model.gato;
+
+public class PrimeiroProjetoJava {
+
+	public static void main(String[]args) {
+		gato gato = new gato();
+		
+	System.out.println(gato);
+	}
+	
+
+}
